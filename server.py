5be11e01bdad7c62125831c63d4e68de98aa9ecc
Radhika Khurana <mail.radhikakhurana@gmail.com>
2026-05-31 18:30:41 -0400

    no render

--- server.py
+++ server.py
@@ -115,5 +115,4 @@ def run(
 
 
 if __name__ == "__main__":
-    import os
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
+    uvicorn.run(app, host="127.0.0.1", port=8000)
